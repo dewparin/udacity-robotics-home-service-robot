b671f919a8deeaca6a2e4d4ce42a2965d0acc6ce
Dew Parin <parinsura@gmail.com>
2022-10-13 21:34:54 +0700

    Use relative path

--- catkin_ws/src/scripts/pick_objects.sh
+++ catkin_ws/src/scripts/pick_objects.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"/home/workspace/catkin_ws/src/map/base.world\"" &
+WS="$(cd $(dirname "$0")/../..> /dev/null && pwd)"
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$WS/src/map/base.world\"" &
 sleep 15
 
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=\"/home/workspace/catkin_ws/src/map/map.yaml\" initial_pose_a:=-1.625" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=\"$WS/src/map/map.yaml\" initial_pose_a:=-1.625" &
 sleep 15
 
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
